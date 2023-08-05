8005d836d26f8a3fad4ad5d910735c50cb9de1f9
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-08-05 18:23:38 +0200

    Add basic logic

--- Custom_Select/src/Select.tsx
+++ Custom_Select/src/Select.tsx
@@ -1,4 +1,5 @@
 import styles from "./select.module.css"
+import { useState } from "react"
 
 type SelectOption = {
     label: string
@@ -9,28 +10,61 @@ type SelectProps = {
     value?: SelectOption
     onChange: (value: SelectOption | undefined) => void
     options: SelectOption[]
-    
-    
+
+
 
 
 }
 
 
 export function Select({ value, onChange, options }: SelectProps) {
+    const [isOpen, setIsOpen] = useState(false)
+    const [highlightedIndex, setHighlightedIndex] = useState(0)
+
+    function clearOptions() {
+        onChange(undefined)
+    }
+
+    function selectOption(option: SelectOption) {
+        onChange(option)
+    }
+
+    function isOptionSelected(option: SelectOption) {
+        return option === value
+    }
+
     return (
-        <div tabIndex={0} className={styles.container}>
-            <span className={styles.value}>Value</span>
-            <button className={styles["clear-btn"]}>&times;</button>
+        <div
+            onBlur={() => setIsOpen(false)}
+            onClick={() => setIsOpen(prev => !prev)}
+            tabIndex={0} className={styles.container}>
+            <span className={styles.value}>{value?.label}</span>
+            <button onClick={
+                e => {
+                    e.stopPropagation()
+                    clearOptions()
+                }
+            } className={styles["clear-btn"]}>&times;</button>
             <div className={styles.divider}></div>
             <div className={styles.caret}></div>
-            <ul className={`${styles.options} ${styles.show}`}>
-                {options.map(option => (
-                    <li key={option.label} className={styles.option}>
+            <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
+                {options.map((option, index) => (
+                    <li
+                        onClick={e => {
+                            e.stopPropagation()
+                            selectOption(option)
+                            setIsOpen(false)
+                        }}
+                        onMouseEnter={() => setHighlightedIndex(index)}
+                        key={option.label}
+                        className={`${styles.option} ${isOptionSelected(option) ? styles.selected : ""}
+                        ${index===highlightedIndex ? styles.highlighted: ""}
+                        `} >
                         {option.label}
                     </li>
                 ))}
             </ul>
         </div>
-            
+
     )
 }
